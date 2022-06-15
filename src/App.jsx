beb5b613449f656ad1a19641a6e4ab2231f94bf4
Oth-mane1 <othmane.ouirdy@gmail.com>
2022-06-16 00:08:21 +0100

    import login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+// import RippleButton from "./components/RippleButton";
+import Login from "./components/Login";
+import React from "react";
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	return (
+		<div className="App">
+			<Login />
+		</div>
+	);
 }
 
 export default App;
