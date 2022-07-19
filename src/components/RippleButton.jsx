d6d713902fe3b34f5b817fdbfe8e1d88d4194a7a
Oth-mane1 <othmane.ouirdy@gmail.com>
2022-07-19 22:37:40 +0100

    Add loading icon

--- src/components/RippleButton.jsx
+++ src/components/RippleButton.jsx
@@ -13,7 +13,7 @@ export default function RippleButton(props) {
 	return (
 		<button
 			id={props?.id}
-			className={"ripple-btn " + props?.class}
+			className={"ripple-btn " + props?.className}
 			style={props?.style}
 			type={props?.id || "button"}
 			onClick={(e) => {
@@ -22,12 +22,22 @@ export default function RippleButton(props) {
 				if (!props?.click) return;
 
 				if (Array.isArray(props?.click)) {
-					props?.click.map((clk) => clk());
+					return props?.click.map((clk) => clk());
 				}
 
 				if (props?.click instanceof Function) return props?.click();
 			}}
 		>
+
+			{
+				!props?.isLoading ?
+					""
+					: <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+						<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+						<path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+					</svg>
+			}
+
 			{props?.children}
 		</button>
 	);
