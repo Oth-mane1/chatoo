ddd4d212b40c4f47af9353039ec40be9e615236c
Oth-mane1 <othmane.ouirdy@gmail.com>
2022-08-02 19:39:09 +0100

    Implement a header for the app

--- src/components/app/Header.jsx
+++ src/components/app/Header.jsx
@@ -1,9 +1,47 @@
-import { useEffect } from "react"
+import { ChevronDown } from './../utils/icons'
+import { Menu, Transition } from '@headlessui/react'
+import { Fragment } from "react";
+
+const OCHAT_LOGO = require('./../../images/logo/ochatLogo.png');
+const USER_AVATAR = false ? '' : require('./../../images/avatars/men_smiling_avatar.png');
 
 function Header(props) {
     return (
-        <>
-        </>
+        <div className="h-full flex justify-between py-0.5 px-3">
+            <a href="/" className="m-1 hover:drop-shadow-xl transition delay-75">
+                <img className="h-full" src={OCHAT_LOGO} alt="ochat logo" />
+            </a>
+
+            <div className="m-1">
+                <Menu as="div" className="h-full relative inline-block text-left">
+                    <Menu.Button className={'h-full flex items-center transition delay-75 hover:drop-shadow-xl rounded-full'} >
+                        <img className="h-full" src={USER_AVATAR} alt="user avatar" />
+                        <ChevronDown className="w-5 h-5"/>
+                    </Menu.Button>
+                    <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                    >
+                        <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">                            
+                            <div className="px-1 py-1 ">
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <button className={'w-full flex items-center rounded-md px-2 py-2 text-sm hover:bg-blue-jelly hover:text-white'} >
+                                            Settings
+                                        </button>
+                                    )}
+                                </Menu.Item>
+                            </div>
+                        </Menu.Items>
+                    </Transition>
+                </Menu>
+            </div>
+        </div>
     )
 }
 
