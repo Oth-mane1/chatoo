3faf2ec0766bac1b556336d8fab0db3ae4a51a0d
Oth-mane1 <othmane.ouirdy@gmail.com>
2022-08-04 00:48:01 +0100

    add new icons

--- src/components/utils/icons.jsx
+++ src/components/utils/icons.jsx
@@ -4,6 +4,12 @@ function ChevronDown(props) {
     )
 }
 
+function Search(props) {
+    return (
+        <svg className={props.className || "w-6 h-6"} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>)
+}
+
 export {
-    ChevronDown
+    ChevronDown,
+    Search
 }
