1dfeec0b3d4724b6d65e6f1e1fb70e3fc2e32b42
Oth-mane1 <othmane.ouirdy@gmail.com>
2022-08-04 00:52:09 +0100

    create the header component

--- src/components/app/Header.jsx
+++ src/components/app/Header.jsx
@@ -16,7 +16,7 @@ function Header(props) {
                 <Menu as="div" className="h-full relative inline-block text-left">
                     <Menu.Button className={'h-full flex items-center transition delay-75 hover:drop-shadow-xl rounded-full'} >
                         <img className="h-full" src={USER_AVATAR} alt="user avatar" />
-                        <ChevronDown className="w-5 h-5"/>
+                        <ChevronDown className="w-5 h-5" />
                     </Menu.Button>
                     <Transition
                         as={Fragment}
@@ -27,16 +27,17 @@ function Header(props) {
                         leaveFrom="transform opacity-100 scale-100"
                         leaveTo="transform opacity-0 scale-95"
                     >
-                        <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">                            
-                            <div className="px-1 py-1 ">
-                                <Menu.Item>
-                                    {({ active }) => (
-                                        <button className={'w-full flex items-center rounded-md px-2 py-2 text-sm hover:bg-blue-jelly hover:text-white'} >
-                                            Settings
-                                        </button>
-                                    )}
-                                </Menu.Item>
-                            </div>
+                        <Menu.Items className="absolute right-0 p-1 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-2xl bg-white shadow-xl shadow-gray-200 ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <Menu.Item>
+                                <button className={'w-full flex items-center rounded-full px-2 py-2 text-sm hover:bg-blue-jelly hover:text-white'} >
+                                    Profil
+                                </button>
+                            </Menu.Item>
+                            <Menu.Item>
+                                <button className={'w-full flex items-center rounded-full px-2 py-2 text-sm hover:bg-blue-jelly hover:text-white'} >
+                                    Settings
+                                </button>
+                            </Menu.Item>
                         </Menu.Items>
                     </Transition>
                 </Menu>
