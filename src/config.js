a1ac09dae1fb80f7a11572d734e70edbe25b9c88
Oth-mane1 <othmane.ouirdy@gmail.com>
2022-10-14 23:27:00 +0100

    Add config file to storevarious configs

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDTgLipZUo5pZVB-0s6224RP2hM4eIqb1Q",
+    authDomain: "ochat-11.firebaseapp.com",
+    projectId: "ochat-11",
+    storageBucket: "ochat-11.appspot.com",
+    messagingSenderId: "88213140416",
+    appId: "1:88213140416:web:ea6d9ae78aa45bdf0b8e25",
+    measurementId: "G-L0LK723XKG"
+};
+
+export { firebaseConfig }
