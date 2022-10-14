51778b0da9751cabcb1d224a69d8669441981aaf
Oth-mane1 <othmane.ouirdy@gmail.com>
2022-10-14 23:29:42 +0100

    Add emoji picker

--- src/components/app/chat/Chat.jsx
+++ src/components/app/chat/Chat.jsx
@@ -1,11 +1,42 @@
 import { Emoji, PaperAirplane } from "../../utils/icons";
 import UserChatHeader from "./ChatHeader";
 import Message from "./Message";
+import { createPopup } from '@picmo/popup-picker';
+import { EmojiPicker, EmojiSelection } from 'picmo';
+import { useEffect } from "react";
+import { useRef } from "react";
+
 function Chat(props) {
     const { userSelected } = props
-    
+    const typedMessage = document.querySelector('#typedMessage')
+
+    const picker = createPopup({
+        animate: true,
+        theme: "light",
+    }, {
+        triggerElement: document.querySelector('#emojiPickerBtn'),
+        referenceElement: document.querySelector('#refEmojiContainer'),
+        hideOnClickOutside: true,
+        hideOnEscape: true,
+        showCloseButton: true,
+        hideOnEmojiSelect: false,
+    })
+
+    picker.addEventListener("emoji:select", sel => {
+        typedMessage.textContent += sel.emoji
+        // console.log('Selected emoji: ', sel.emoji);
+    })
+
+    function triggerEmojiPicker() {
+        picker.open()
+    }
+
+    useEffect(() => {
+        if (typedMessage) typedMessage.textContent = "";
+    })
+
     return (
-        <div className="relative p-3 h-full">
+        <div id={"refEmojiContainer"} className="relative p-3 h-full">
             {
                 userSelected ?
                     <>
@@ -18,10 +49,10 @@ function Chat(props) {
                             </div>
                         </div>
                         <div className="flex items-center relative w-full px-1 py-2 bg-white rounded-full">
-                            <button type="button" className="absolute left-2">
+                            <button id="emojiPickerBtn" onClick={triggerEmojiPicker} type="button" className="absolute left-2">
                                 <Emoji className="w-10 h-10 hover:stroke-blue-jelly transition duration-200" />
                             </button>
-                            <textarea className="w-full pl-12 py-3 mr-4 shadow transition duration-200 outline-none focus:ring-2 ring-offset-1 ring-blue-jelly bg-gray-200 rounded-full resize-none no-scrollbar" placeholder="Message..." rows="1"></textarea>
+                            <textarea id="typedMessage" className="w-full pl-12 py-3 mr-4 shadow transition duration-200 outline-none focus:ring-2 ring-offset-1 ring-blue-jelly bg-gray-200 rounded-full resize-none no-scrollbar" placeholder="Message..." rows="1"></textarea>
                             {/* <input type="text" /> */}
                             <button type="button" className="p-2 rounded-full transition duration-200 bg-blue-jelly hover:bg-blue-iceberg text-white outline-none focus:ring-2 ring-offset-1 ring-blue-jelly">
                                 <PaperAirplane className="w-8 h-8 pb-1 rotate-45" />
